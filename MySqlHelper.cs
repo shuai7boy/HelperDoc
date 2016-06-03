6da577f0eddd2283aea758a720b66b9b905472b2
RYJ <2636922684@qq.com>
2016-06-03 09:13:07 +0800

    帮助文档
    帮助文档大全

---
+++ MySqlHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace MySql练习
+{
+   public static class SqlHelper
+    {
+       private static string strConn = ConfigurationManager.ConnectionStrings["sql"].ConnectionString;
+       //ExecuteNonquery
+       public static int ExecuteNonquery(string sql, CommandType cmdType, params MySqlParameter[] pms)
+       {
+           using (MySqlConnection conn = new MySqlConnection(strConn))
+           {
+               using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+               {
+                   cmd.CommandType = cmdType;
+                   if (pms != null)
+                   {
+                       cmd.Parameters.AddRange(pms);
+
+                   }
+                   conn.Open();
+                   return cmd.ExecuteNonQuery() ;
+               }
+           }
+       }
+       //ExecuteScalar
+       public static object ExecuteScalar(string sql, CommandType cmdType, params MySqlParameter[] pms)
+       {
+           using (MySqlConnection conn = new MySqlConnection(strConn))
+           {
+
+               using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+               {
+                   cmd.CommandType = cmdType;
+                   if (pms != null)
+                   {
+                       cmd.Parameters.AddRange(pms);
+                   }
+                   conn.Open();
+                   return cmd.ExecuteScalar();
+               }
+           }
+       }
+       //DataReader
+       public static MySqlDataReader ExecuteReader(string sql, CommandType cmdType, params MySqlParameter[] pms)
+       {
+           MySqlConnection conn = new MySqlConnection(strConn);
+           using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+           {
+               cmd.CommandType = cmdType;
+               if (pms != null)
+               {
+                   cmd.Parameters.AddRange(pms);
+               }
+               try
+               {
+                   if (conn.State == ConnectionState.Closed)
+                   {
+                       conn.Open();
+                   }
+                   return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+               }
+               catch
+               {
+                   conn.Close();
+                   conn.Dispose();
+                   throw;
+               }
+           }
+       }
+       //DataTable 
+       public static DataSet ExecuteDataTable(string sql, CommandType cmdType, params MySqlParameter[] pms)
+       {                   
+           DataSet dt = new DataSet();
+           using (MySqlDataAdapter adapter = new MySqlDataAdapter(sql, strConn))
+           {
+               adapter.SelectCommand.CommandType = cmdType;
+               if (pms != null)
+               {
+                   adapter.SelectCommand.Parameters.AddRange(pms);
+               }
+               adapter.Fill(dt);
+           }
+           return dt;
+       }
+
+    }
+}
---
+++ SQLiteHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Food.DAL
+{
+   public static class SQLiteHelper
+    {
+        public static string strConn = ConfigurationManager.ConnectionStrings["sqlite"].ConnectionString;
+        //ExecuteNonquery
+        public static int ExecuteNonquery(string sql, params SQLiteParameter[] pms)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(strConn))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        //ExecuteScalar
+        public static object ExecuteScalar(string sql, params SQLiteParameter[] pms)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(strConn))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+        //ExecuteReader
+        public static SQLiteDataReader ExecuteReader(string sql, params SQLiteParameter[] pms)
+        {
+            SQLiteConnection conn = new SQLiteConnection(strConn);
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+            {
+                if (pms != null)
+                {
+                    cmd.Parameters.AddRange(pms);
+                }
+                try
+                {
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    return cmd.ExecuteReader();
+                }
+                catch
+                {
+                    conn.Close();
+                    conn.Dispose();
+                    throw;
+                }
+            }
+        }
+        //ExecuteDataTable
+        public static DataTable ExecuteDataTable(string sql, params SQLiteParameter[] pms)
+        {
+           // SQLiteConnection conn = new SQLiteConnection(strConn);
+           // conn.Open();
+            DataTable dt = new DataTable();
+            using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql, strConn))
+            {
+                if (pms != null)
+                {
+                    adapter.SelectCommand.Parameters.AddRange(pms);
+                }
+                adapter.Fill(dt);
+            }
+           // conn.Close();
+            return dt;
+        }
+    }
+}
